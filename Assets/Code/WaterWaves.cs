f88db79993455972163164157991ee3391fdb1c5
coleh2 <cbhreads2@gmail.com>
2019-11-06 06:21:03 -0500

    bump map based waves

--- Assets/Code/WaterWaves.cs
+++ Assets/Code/WaterWaves.cs
@@ -9,18 +9,43 @@ public class WaterWaves : MonoBehaviour
     [SerializeField]
     public float speed = 1;
     [SerializeField]
-    public int resolution = 11;
-
-    public Color darkWater;
-    public Color lightWater;
+    public int resolution = 512;
+    public int tileSize = 16;
+    private float size = 11;
 
     private Mesh mesh;
     private float[] initialHeights;
+    private int tileCount;
    
     void Start()
     {
-        this.mesh = gameObject.GetComponent<MeshFilter>().mesh;
-        this.initialHeights = new float[mesh.vertices.Length];
+        size = gameObject.transform.localScale.x;
+
+        //create tiles
+        float tileRelativeScale = tileSize/gameObject.transform.localScale.x;
+        
+        int tileSideCount = (int)gameObject.transform.localScale.x/tileSize;
+
+        tileCount = tileSideCount*tileSideCount;
+        for(int i = 0; i < tileCount; i++) {
+            int xCoord = (int)Mathf.Floor(i/resolution);
+            int yCoord = i%resolution;
+
+            GameObject copyTile = Instantiate(gameObject);
+            Debug.Log(tileRelativeScale);
+            copyTile.transform.localScale.Set(tileRelativeScale, 1f, tileRelativeScale);
+            Debug.Log(copyTile.transform.localScale);
+            copyTile.transform.localPosition.Set(xCoord-tileSideCount+1, 0f, yCoord-tileSideCount+1);
+
+            copyTile.transform.parent = gameObject.transform;
+            copyTile.GetComponent<WaterWaves>().enabled = false;
+        }
+
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        
+        this.mesh = gameObject.GetComponent<MeshFilter>().mesh = gameObject.GetComponent<MeshCollider>().sharedMesh;
+
+        this.initialHeights = new float[resolution*resolution];
         for(int i = 0; i < initialHeights.Length; i++) {
             initialHeights[i] = Random.Range(0f, magnitude);
         }
@@ -28,32 +53,48 @@ public class WaterWaves : MonoBehaviour
 
     void Update()
     {
-        Vector3[] vertices = mesh.vertices;
-        Color[] vertexColors = new Color[vertices.Length];
+        Vector3[] vertices = new Vector3[resolution*resolution];
         for(int i = 0; i < vertices.Length; i++) {
-            //each vertex goes up and down between its random initial value and 0
+            //each point goes up and down between its random initial value and 0
             vertices[i].y = Mathf.PingPong(Time.time*speed, initialHeights[i]);
         }
 
-        //set heightmap
-        mesh.vertices = vertices;
-        //set texture for lake
-        gameObject.GetComponent<MeshRenderer>().material.mainTexture = BuildWaterTexture(vertices, resolution, magnitude);
+        for(int i = 0; i < tileCount; i++) {
+            Debug.Log(gameObject.transform.GetChild(i).transform.localScale);
+            gameObject.transform.GetChild(i).GetComponent<MeshRenderer>().material.SetTexture("_BumpMap", BuildWaterBumpMap(vertices, resolution, magnitude));
+        }
+        gameObject.GetComponent<MeshRenderer>().material.SetTexture("_BumpMap", BuildWaterBumpMap(vertices, resolution, magnitude));
+        
+        Resources.UnloadUnusedAssets();
     }
 
-    private Texture2D BuildWaterTexture(Vector3[] vertices, int resolution, float magnitude) {
-
+    private Texture2D BuildWaterBumpMap(Vector3[] vertices, int resolution, float magnitude) {
         Color[] colorMap = new Color[vertices.Length];
         for (int i = 0; i < vertices.Length; i++) {
-            // assign as color a shade of proportional to the height value
-            colorMap [i] = Color.Lerp(darkWater, lightWater, vertices[i].y/magnitude);
+            //calculate differences from one pixel to another
+            float xDown = 0;
+            float xUp = 0;
+            if(i%resolution > 0) xDown = vertices[i-1].y;
+            else if(i%resolution < resolution - 1) xUp = vertices[i+1].y;
+
+            float yDown = 0;
+            float yUp = 0;
+            if(i-resolution > 0) yDown = vertices[i-resolution].y;
+            else if(i+resolution < resolution*resolution) yUp = vertices[i+resolution].y;
+
+            float xDelta = ((xUp-xDown)+1)*0.5f;
+            float yDelta = ((yUp-yDown)+1)*0.5f;
+
+            colorMap[i] = new Color(xDelta, yDelta, 1f, yDelta);
         }
 
-        // create a new texture and set its pixel colors
+        // create a new texture and set its pixel colors to the calculated deltas
         Texture2D tileTexture = new Texture2D (resolution, vertices.Length / resolution);
         tileTexture.wrapMode = TextureWrapMode.Clamp;
         tileTexture.SetPixels(colorMap);
-        tileTexture.Apply ();
+        tileTexture.Apply();
+
+        colorMap = null;
 
         return tileTexture;
     }
