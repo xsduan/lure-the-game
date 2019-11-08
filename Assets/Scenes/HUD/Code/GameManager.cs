343df524112eea03b2ed91f57bf9ff210b806ab8
Shane Duan <s_duan@u.pacific.edu>
2019-11-07 20:57:11 -0800

    Clean up Game Manager

--- Assets/Scenes/HUD/Code/GameManager.cs
+++ Assets/Scenes/HUD/Code/GameManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // the names are prety obvious if you ask me
     public GameObject pauseMenu;
     private int pauseMenuActive = 0;
     public GameObject MainMenu;
@@ -16,13 +15,9 @@ public class GameManager : MonoBehaviour
 
     public Camera Camera;
     public Camera MenuCamera;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
-        //you can tell what this does
-        //   ||
-        //   \/
         Time.timeScale = 0;
         pauseMenu.SetActive(false);
         Camera.enabled = false;
@@ -33,33 +28,26 @@ public class GameManager : MonoBehaviour
         ShopCanvas.SetActive(false);
         InventoryCanvas.SetActive(false);
         IsMenuActive = true;
-        //   /\
-        //   ||
-        //you can tell what this does
-
-
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Escape)) //check for ESC press
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (IsMenuActive == false) //inafisieant way for not having the pause menu open in the main menu
+            if (IsMenuActive == false)
             {
-                if (pauseMenuActive == 1) //because i forgot bool exists and am too lazy to change it
+                if (pauseMenuActive == 1)
                 {
                     pauseMenu.SetActive(false);
                     pauseMenuActive = 0;
-                    Time.timeScale = 1; //unfreeses the game
+                    Time.timeScale = 1;
                 }
 
                 else if (pauseMenuActive == 0)
                 {
                     pauseMenu.SetActive(true);
                     pauseMenuActive = 1;
-                    Time.timeScale = 0;// freeses time     ZA WARLDO!
+                    Time.timeScale = 0;
                 }
             }
             
@@ -71,51 +59,51 @@ public class GameManager : MonoBehaviour
     {
         pauseMenu.SetActive(false); 
         pauseMenu.SetActive(false); 
-        pauseMenuActive = 0; //because i forgot bool exists and am too lazy to change it
+        pauseMenuActive = 0;
         Time.timeScale = 1;
     }
 
     public void StartGame()
     {
-        Time.timeScale = 1; //un freses the game time
-        Camera.enabled = true;//turns off the normal camera
-        MenuCamera.enabled = false;//turns off the menu camera
-        MainMenu.SetActive(false);//you can tell what this does
-        HUD.SetActive(true);//you can tell what this does
+        Time.timeScale = 1;
+        Camera.enabled = true;
+        MenuCamera.enabled = false;
+        MainMenu.SetActive(false);
+        HUD.SetActive(true);
         IsMenuActive = false;
     }
 
     public void mainMenuActive()
     {
-        Time.timeScale = 0; //freeses the game time    ZA WORLDO!
-        Camera.enabled = false;//turns off the normal camera
-        MenuCamera.enabled = true;//turns on the menu camera
-        HUD.SetActive(false);         //you can tell what this does
-        pauseMenu.SetActive(false);   //you can tell what this does
-        MainMenu.SetActive(true);     //you can tell what this does
+        Time.timeScale = 0;
+        Camera.enabled = false;
+        MenuCamera.enabled = true;
+        HUD.SetActive(false);
+        pauseMenu.SetActive(false);
+        MainMenu.SetActive(true);
         IsMenuActive = true;
     }
 
-    public void ExitGame() //you can tell what this does
+    public void ExitGame()
     {
         Application.Quit();
     }
 
-    public void ActivateShop() //activates the shop and deactivates the other 2
+    public void ActivateShop()
     {
         MapCanvas.SetActive(false);
         ShopCanvas.SetActive(true);
         InventoryCanvas.SetActive(false);
     }
 
-    public void ActivateMap()//activates the map and deactivates the other 2
+    public void ActivateMap()
     {
         MapCanvas.SetActive(true);
         ShopCanvas.SetActive(false);
         InventoryCanvas.SetActive(false);
     }
 
-    public void ActivateInventory()//activates the inventory and deactivates the other 2
+    public void ActivateInventory()
     {
         MapCanvas.SetActive(false);
         ShopCanvas.SetActive(false);
