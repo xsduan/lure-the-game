452b1a1b50ecc464bdd355971fc9d417beebd821
Jeff Atkinson <jatkinson20@gmail.com>
2019-11-08 22:34:20 -0600

    Bugged Fish Spawn
    Fish successfully spawn but do not spawn at the set location.

---
+++ Assets/Scenes/Lake/Code/SpawnFish.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFish : MonoBehaviour
+{
+    #region Class Variables
+    public GameObject fish;
+    public float range = 2.0f;
+    public float depth = 10f;
+    public int popCap = 5;
+    private int pop;
+    #endregion
+
+    void OnTriggerStay ()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        Vector3 pos;
+        //if spawn has not hit capacity
+        if (pop < popCap){
+
+            //Calculate random position within specified range and depth around spawn point.
+            //pos = Random.insideUnitCircle * range;
+            //pos.y = -Random.Range(-depth, -1);
+            //pos += transform.position;
+
+            //spawn fish
+            Object.Instantiate(fish, transform.position, Quaternion.identity);
+            pop++;
+        }
+    }
+}
---
+++ Assets/Scenes/Lake/Code/SpawnZone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    public GameObject fish;
+    int pop;
+    public int popCap;
+
+    void Update() {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Spawn() {
+        Vector3 pos;
+        //if spawn has not hit capacity
+        if (pop < popCap) {
+
+            //Calculate random position within specified range and depth around spawn point.
+            //pos = Random.insideUnitCircle * range;
+            //pos.y = -Random.Range(-depth, -1);
+            //pos += transform.position;
+
+            //spawn fish
+            GameObject obj = Object.Instantiate(fish, transform.position, Quaternion.identity);
+            Debug.Log(transform.position);
+            Debug.Log(obj.transform.position);
+            obj.transform.position = new Vector3(0,0,0);
+            pop++;
+        }
+    }
+}
