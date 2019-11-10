a425aa02118dfa2f839344539b614430c710eb15
Jeff Atkinson <jatkinson20@gmail.com>
2019-11-10 10:01:56 -0600

    Add comments to SpawnFish script
    Also changed depth to subtract from player position so that the serialized field can be a positive number.

--- Assets/Scenes/Lake/Code/SpawnFish.cs
+++ Assets/Scenes/Lake/Code/SpawnFish.cs
@@ -14,9 +14,15 @@ public class SpawnFish : MonoBehaviour
     private GameObject[] fishArray;
     #endregion
 
+    //TODO: Select fish to be spawned using probabilities instead of serialized
+    //      GameObject field.
+
     void Start () {
+
+        //Populate area with fish at start
         fishArray = new GameObject[population];
         for (int i = 0; i < population; i++) {
+
             if (fishArray[i] == null) {
                 fishArray[i] = Spawn();
             }
@@ -24,18 +30,24 @@ public class SpawnFish : MonoBehaviour
     }
 
     void Update () {
+
+        //Each fish that is too far away from the player gets depawned and a new fish spawns within range
         for (int i = 0; i < population; i++) {
-            if (Vector3.Distance(transform.position, fishArray[i].transform.position) >= despawnDistance) {
-                Object.Destroy(fishArray[i]);
+            GameObject currentFish = fishArray[i];
+
+            if (Vector3.Distance(transform.position, currentFish.transform.position) >= despawnDistance) {
+                Object.Destroy(currentFish);
+
                 fishArray[i] = Spawn();
             }
         }
     }
 
-    GameObject Spawn()
-    {
+    GameObject Spawn() { 
+
+        //Determine random spawn position based on player's position, then spawn new fish
         float randX = transform.position.x + Random.Range(-range, range);
-        float randY = transform.position.y + Random.Range(1, depth);
+        float randY = transform.position.y - Random.Range(1, depth);
         float randZ = transform.position.z + Random.Range(-range, range);
 
         Vector3 spawnPos = new Vector3(randX, randY, randZ);
