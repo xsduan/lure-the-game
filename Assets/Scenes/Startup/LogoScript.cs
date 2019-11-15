e77302d85c2afd607f606f136d7f6a6ba5534963
Alex <57309096+EgoOrb@users.noreply.github.com>
2019-11-15 15:39:12 -0500

    Added line casting and startup screen

---
+++ Assets/Scenes/Startup/LogoScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoScript : MonoBehaviour
+{
+    public string MainMenuSceneName;
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(MainMenuSceneName);
+    }
+}
