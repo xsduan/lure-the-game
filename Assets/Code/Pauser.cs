9c7abe8738f641a14c7f4b11b3922b70f72c23af
Shane Duan <s_duan@u.pacific.edu>
2019-11-29 22:42:36 -0800

    Move Pauser and Swapper to top layer code folder
    This is because they're more "utility" than implementation.

---
+++ Assets/Code/Pauser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Pauser
+{
+    public enum PauseState { PAUSED, ACTIVE }
+
+    private static float lastActiveTimeScale;
+    private static readonly Swapper<PauseState, float> _pauseStatus;
+    public static PauseState PauseStatus
+    { get => _pauseStatus.CurrentKey; set => _pauseStatus.CurrentKey = value; }
+
+    static Pauser()
+    {
+        _pauseStatus = new Swapper<PauseState, float>((scale, active) => { if (active) Time.timeScale = scale; });
+        _pauseStatus[PauseState.PAUSED] = 0;
+        _pauseStatus[PauseState.ACTIVE] = Time.timeScale;
+    }
+}
